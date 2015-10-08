e95a10578c842abdcfe6854e221b38681df10d00
ylagorebollar <ylagorebollar@atanasoff04.danielcastelao.org>
2015-10-08 09:04:04 +0200

    Comentario

---
+++ src/proyectoalfa/Proyectoalfa.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+public class Proyectoalfa {
+
+    public static void main(String[] args) {
+     float cambio= 1.2f,euros;
+     Scanner dolares=new Scanner(System.in);
+     System.out.println("cambio= "+cambio);//Visualizar cambio
+     System.out.println("euros");
+     euros=dolares.nextFloat();
+     System.out.println("Dolares= "+euros*cambio+"$");
+    
+    
+
+    }
+    
+}
