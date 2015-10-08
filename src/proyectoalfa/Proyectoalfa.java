01dd0ae53232b47ec81bcf7bd70314fd9f5d3b51
YasminLago <ylagorebollar@danielcastelao.org>
2015-10-08 09:13:30 +0200

    Update Proyectoalfa.java

--- src/proyectoalfa/Proyectoalfa.java
+++ src/proyectoalfa/Proyectoalfa.java
@@ -1,12 +1,5 @@
 package proyectoalfa;
 import java.util.Scanner;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 public class Proyectoalfa {
 
     public static void main(String[] args) {
