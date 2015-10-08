401000dbe68b685dd18c6f408db6506ed5952272
ylagorebollar <ylagorebollar@atanasoff04.danielcastelao.org>
2015-10-08 09:06:55 +0200

    Inserción comentario: nueva variable

--- src/proyectoalfa/Proyectoalfa.java
+++ src/proyectoalfa/Proyectoalfa.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class Proyectoalfa {
 
     public static void main(String[] args) {
-     float cambio= 1.2f,euros,resultado;
+     float cambio= 1.2f,euros,resultado;//Nueva variable
      Scanner dolares=new Scanner(System.in);
      System.out.println("cambio= "+cambio);//Visualizar cambio
      System.out.println("euros");
