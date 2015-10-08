717143c0b3bfd80fcc1a736c6b705bb8c2ee2c15
ylagorebollar <ylagorebollar@atanasoff04.danielcastelao.org>
2015-10-08 09:06:15 +0200

    Cambio en linea 18

--- src/proyectoalfa/Proyectoalfa.java
+++ src/proyectoalfa/Proyectoalfa.java
@@ -10,12 +10,13 @@ import java.util.Scanner;
 public class Proyectoalfa {
 
     public static void main(String[] args) {
-     float cambio= 1.2f,euros;
+     float cambio= 1.2f,euros,resultado;
      Scanner dolares=new Scanner(System.in);
      System.out.println("cambio= "+cambio);//Visualizar cambio
      System.out.println("euros");
      euros=dolares.nextFloat();
-     System.out.println("Dolares= "+euros*cambio+"$");
+     resultado=euros*cambio;
+     System.out.println("Dolares= "+resultado+"$");
     
     
 
